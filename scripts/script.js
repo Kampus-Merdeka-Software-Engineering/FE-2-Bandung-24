cfa0747ca71ac8e8ba4187b4b3c219786c83ba3c
mohatami_ <hatamitekno@gmail.com>
2023-11-10 10:28:24 +0700

    add header

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+// navbar function
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
